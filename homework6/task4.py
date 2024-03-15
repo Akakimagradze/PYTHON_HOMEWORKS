4c76fefc1d7b9839b6cb3b7663f508bd8c1226ec
Akakimagradze <akomagradze2@gmail.com>
2024-03-15 14:00:24 +0400

    Updated task4.py file

--- homework6/task4.py
+++ homework6/task4.py
@@ -7,7 +7,7 @@ i = 1
 while i <= userNum:
     j = 1
     while j <= i:
-        print(j, end="")
+        print(j, end=" ")
         j += 1
     print()
     i += 1
@@ -16,7 +16,7 @@ i = userNum - 1
 while i >= 1:
     j = 1
     while j <= i:
-        print(j, end="")
+        print(j, end=" ")
         j += 1
     print()
     i -= 1
