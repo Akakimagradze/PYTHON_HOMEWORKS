8576d791679588eb1bdf8416612567e609004742
Akakimagradze <akomagradze2@gmail.com>
2024-03-18 14:45:35 +0400

    Corrected homework7 mistakes

--- homework7/task1.py
+++ homework7/task1.py
@@ -1,5 +1,3 @@
-import sys
-
 user_num = int(input("Enter number(0-19): "))
 
 while user_num < 0 or user_num >= 20:
@@ -13,6 +11,4 @@ while i < user_num:
     a, b = b, a + b
     print(a, end=" ")
     i += 1
-
-sys.exit(1)
 
